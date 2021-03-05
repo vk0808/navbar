e8043673da754890eb16dce96036da4f7f2e15e9
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-05 22:26:35 +0530

    added links, social icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { FaBars, FaTwitter } from 'react-icons/fa';
+import { FaBars, FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
@@ -11,8 +11,19 @@ const Navbar = () => {
           <FaBars />
         </button>       
       </div>
-      <div className="links-container show-container"></div>
-      <ul className="social-icons"></ul>
+      <div className="links-container show-container">
+        <ul className="links">
+          <li><a href="#">home</a></li>
+          <li><a href="#">about</a></li>
+          <li><a href="#">contact</a></li>
+          <li><a href="#">product</a></li>
+        </ul>
+      </div>
+      <ul className="social-icons">
+        <li><a href="https://www.twitter.com"><FaTwitter /></a></li>
+        <li><a href="https://www.facebook.com"><FaFacebook /></a></li>
+        <li><a href="https://www.instagram.com"><FaInstagram /></a></li>
+      </ul>
       </div>
     </nav>
   );
