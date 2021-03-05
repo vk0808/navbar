fa1b8882a0cee741a7da3cd3939c7af337bc2404
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-05 22:20:00 +0530

    added nav-header

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect, useRef } from "react";
+import { FaBars, FaTwitter } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <p>Hello</p>
+    <nav>
+      <div className="nav-center">
+      <div className="nav-header">
+        <h1>LOGO</h1>
+        <button className="nav-toggle">
+          <FaBars />
+        </button>       
+      </div>
+      <div className="links-container show-container"></div>
+      <ul className="social-icons"></ul>
+      </div>
+    </nav>
   );
 } 
 
