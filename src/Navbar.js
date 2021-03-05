743aff82d44e01ef71ff8868a75bf952b4bc82d9
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-05 23:01:07 +0530

    mapped to data

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { FaBars, FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
-import { data } from './data';
+import { links, social } from './data';
 
 const Navbar = () => {
   return (
@@ -14,16 +14,21 @@ const Navbar = () => {
       </div>
       <div className="links-container show-container">
         <ul className="links">
-          <li><a href="#">home</a></li>
-          <li><a href="#">about</a></li>
-          <li><a href="#">contact</a></li>
-          <li><a href="#">product</a></li>
+          {links.map((link)=> {
+            const {id, url, text} = link;
+            return <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          })}
         </ul>
       </div>
       <ul className="social-icons">
-        <li><a href="https://www.twitter.com"><FaTwitter /></a></li>
-        <li><a href="https://www.facebook.com"><FaFacebook /></a></li>
-        <li><a href="https://www.instagram.com"><FaInstagram /></a></li>
+        {social.map((socialIcon)=> {
+          const {id, url, icon} = socialIcon;
+          return <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        })}
       </ul>
       </div>
     </nav>
--- src/data.js
+++ src/data.js
@@ -13,13 +13,18 @@ export const links = [
   },
   {
     id: 3,
+    url: '/projects',
+    text: 'projects',
+  },
+  {
+    id: 4,
     url: '/contact',
     text: 'contact',
   },
   {
-    id: 4,
-    url: '/product',
-    text: 'product',
+    id: 5,
+    url: '/profile',
+    text: 'profile',
   },
 ]
 
